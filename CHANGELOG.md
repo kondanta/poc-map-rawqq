797128882f7067fed7ababbec7c92e4e186a098e
Taylan Dogan <doganntaylan@gmail.com>
2019-06-04 16:05:42 +0300

    feat: Implement chapter info scraper

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+##  (June 04, 2019)
+Implement manga info parsing from the given link.
+
--- main.go
+++ main.go
@@ -6,21 +6,43 @@ import (
 	"github.com/gocolly/colly"
 )
 
-func main() {
+type ChapterInfo struct {
+	URL          string
+	LastUpdate   string
+	ChapterTitle string
+}
+
+func ChapterInfoExtractor() *[]ChapterInfo {
 	url := "https://hanascan.com/manga-chiyu-mahou-no-machigatta-tsukaikata-senjou-wo-kakeru-kaifuku-youin-raw.html"
+	info := []ChapterInfo{}
 
 	c := colly.NewCollector(
 		colly.AllowedDomains("hanascan.com"),
+		colly.Async(true),
+		colly.MaxDepth(10),
 	)
 
-	c.OnHTML(".container .row .manga-info", func(e *colly.HTMLElement) {
-		fmt.Println(e.ChildText("h1"))
-		fmt.Println(e.ChildText("i"))
-		//fmt.Println(e)
-		e.ForEach("li", func(_ int, elem *colly.HTMLElement) {
-			fmt.Println(elem.Text)
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  "*",
+		Parallelism: 10,
+		Delay:       5,
+	})
+
+	c.OnHTML("#list-chapters p", func(e *colly.HTMLElement) {
+		tmp := ChapterInfo{}
+		// Each P element in list-chapters has 3 spans. Index 0: contains <a>, index 1: contains latest update time. index 2
+		// is not useful
+		e.ForEach("span", func(count int, elem *colly.HTMLElement) {
+			switch count {
+			case 0:
+				tmp.ChapterTitle = elem.ChildAttr("a.chapter", "title")
+				tmp.URL = elem.ChildAttr("a.chapter", "href")
+			case 1:
+				tmp.LastUpdate = elem.ChildText("i time")
+			}
 		})
-		//fmt.Println(e.Text)
+		info = append(info, tmp)
+		fmt.Printf("%+v\n", tmp)
 	})
 
 	c.OnRequest(func(r *colly.Request) {
@@ -29,4 +51,10 @@ func main() {
 
 	// Visiting
 	c.Visit(url)
+	c.Wait()
+	return &info
+}
+
+func main() {
+	ChapterInfoExtractor()
 }
